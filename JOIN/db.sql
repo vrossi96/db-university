4c4da730d49cfb184924106844db485af35d9262
vrossi96 <vrossi396@yahoo.it>
2022-03-10 13:58:39 +0100

    done JOIN 3

--- JOIN/db.sql
+++ JOIN/db.sql
@@ -17,6 +17,14 @@ ORDER BY DG.`name` ASC;
 
 
 -- #3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT T.`name`, T.`surname`, C.`name`
+FROM `teachers` T
+JOIN `course_teacher` CT
+ON T.`id` = CT.`teacher_id`
+JOIN `courses` C
+ON C.`id` = CT.`course_id`
+WHERE T.`id` = 44
+ORDER BY C.`name`;
 
 
 -- #4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
